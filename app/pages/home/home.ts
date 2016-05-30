506f749a819c1209156319d73d976631727b2fe8
Muhammad Syafiq <syafiq@my-sands.com>
2016-05-30 23:56:40 +0800

    Add basic form & style.

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,10 +1,18 @@
 import {Page} from 'ionic-angular';
+import {Camera} from 'ionic-native';
+// import {DatePicker} from 'ionic-native';
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
   constructor() {
-
+    // DatePicker.show({
+    //   date: new Date(),
+    //   mode: 'date'
+    // }).then(
+    //   date => console.log("Got date: ", date),
+    //   err => console.log("Error occurred while getting date:", err)
+    // );
   }
 }
