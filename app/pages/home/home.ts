551b7adcc300fb34248aeca2fc4c8931590c4a59
Muhammad Syafiq <syafiq@my-sands.com>
2016-06-01 01:53:33 +0800

    Make button for date picker & geo testing function.

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,18 +1,57 @@
 import {Page} from 'ionic-angular';
-import {Camera} from 'ionic-native';
-// import {DatePicker} from 'ionic-native';
+import {DatePicker, Geolocation, Camera} from 'ionic-native';
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor() {
-    // DatePicker.show({
-    //   date: new Date(),
-    //   mode: 'date'
-    // }).then(
-    //   date => console.log("Got date: ", date),
-    //   err => console.log("Error occurred while getting date:", err)
-    // );
+  constructor() { }
+
+  getGeolocationData() {
+    Geolocation.getCurrentPosition().then(
+      resp => console.log("Latitude: ", resp.coords.latitude, "Longitude: ", resp.coords.longitude),
+      err => console.error("Error occurred while getting geolocation data:", err)
+    );
+  }
+
+  showDatePicker() {
+    DatePicker.show({
+      date: new Date,
+      mode: 'date'
+    }).then(
+      date => console.log("Got date: ", date),
+      err => console.error("Error occurred while getting date:", err)
+    )
+  }
+
+  showTimePicker() {
+    DatePicker.show({
+      date: new Date,
+      mode: 'time'
+    }).then(
+      date => console.log("Got date: ", date),
+      err => console.error("Error occurred while getting date:", err)
+    )
+  }
+
+  showDateTimePicker() {
+    DatePicker.show({
+      date: new Date,
+      mode: 'datetime'
+    }).then(
+      date => console.log("Got date: ", date),
+      err => console.error("Error occurred while getting date:", err)
+      )
+  }
+
+
+
+  showCamera() {
+    // Camera.getPicture().then((imageData) => {
+    //   // imageData is either a base64 encoded string or a file URI
+    //   // If it's base64:
+    //   let base64Image = "data:image/jpeg;base64," + imageData;
+    // }, (err) => {
+    // });
   }
 }
