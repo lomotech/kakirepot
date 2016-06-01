167fbf5339c55d72aec58b5d90115d1398dc9f70
Muhammad Syafiq <syafiq@my-sands.com>
2016-06-01 20:51:24 +0800

    add email composer. android ada masalah. ios ok.

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,5 +1,5 @@
 import {Page} from 'ionic-angular';
-import {DatePicker, Geolocation, Camera} from 'ionic-native';
+import {DatePicker, Geolocation, Camera, EmailComposer} from 'ionic-native';
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
@@ -41,17 +41,39 @@ export class HomePage {
     }).then(
       date => console.log("Got date: ", date),
       err => console.error("Error occurred while getting date:", err)
-      )
+    )
   }
 
 
 
   showCamera() {
-    // Camera.getPicture().then((imageData) => {
-    //   // imageData is either a base64 encoded string or a file URI
-    //   // If it's base64:
-    //   let base64Image = "data:image/jpeg;base64," + imageData;
-    // }, (err) => {
-    // });
+    let options = {};
+
+    Camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+    });
+  }
+
+  showEmail() {
+    EmailComposer.isAvailable().then(
+      available => console.log('email module run smoothly', available),
+      err => console.error('email module is not available', err)
+    );
+
+    let email = {
+      to: 'max@mustermann.de',
+      cc: 'erika@mustermann.de',
+      bcc: ['john@doe.com', 'jane@doe.com'],
+      attachments: [],
+      subject: 'Cordova Icons',
+      body: 'How are you? Nice greetings from Leipzig',
+      isHtml: true
+    };
+
+    // Send a text message using default options
+    EmailComposer.open(email);
   }
 }
